b9803b0072fa23098373e68ba936146492033a16
bruinbailey <nnyskrying@hotmail.com>
2018-10-26 02:39:32 -0400

    working on static data kinda confused about gradeScale and Calendar

---
+++ src/app/staticdata.ts
@@ -0,0 +1,14 @@
+import { Course, GradeScale, Calendar } from './course';
+
+export const COURSES: Course[] = [
+    {
+        title: 'Web Enterprises',
+        section: 'p422',
+        description: 'Course covering client/server web programming',
+        bookInfo: 'Node.js, MongoDB and Angular Web Development by Dayley, Dayley, $ Dayley',
+        meetingTime: 'M/W 19:30-21:20 in LF105',
+        gradingScale: GradeScale,
+        calendar: Calendar,
+        universityInfo: ''
+    }
+]
