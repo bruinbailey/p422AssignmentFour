87e4d43f0c6e518650c3785418a6432e9ace2f0f
bruinbailey <nnyskrying@hotmail.com>
2018-11-01 13:49:04 -0400

    Fixed display error object Object need to figure out how to get array of objects into static data

--- src/app/staticdata.ts
+++ src/app/staticdata.ts
@@ -8,7 +8,7 @@ export const COURSES: Course[] = [
         bookInfo: 'Node.js, MongoDB and Angular Web Development by Dayley, Dayley, $ Dayley',
         meetingTime: 'M/W 19:30-21:20 in LF105',
         gradingScale: { percentageRange: '90-100', letterGrade: 'A'},
-        calendar: { date: '10-28-2018', description: '', notesLink: ''},
+        calendar: { date: '10-28-2018', description: 'angular lecture', notesLink: 'note link goes here'},
         universityInfo: ''
     },
     {
@@ -18,7 +18,7 @@ export const COURSES: Course[] = [
         bookInfo: 'Elementary Statistics by Namor',
         meetingTime: 'T/Th 16:00-17:30 in PS-115',
         gradingScale: { percentageRange: '80-90', letterGrade: 'B'},
-        calendar: { date: '10-28-2018', description: '', notesLink: ''},
+        calendar: { date: '10-28-2018', description: 'chapter 7', notesLink: 'note link goes here'},
         universityInfo: ''
     }
 
