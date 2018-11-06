7f569c62dc298089d3212724d4232b2ba1fe2ba2
bruinbailey <nnyskrying@hotmail.com>
2018-11-06 11:32:20 -0500

    did stuff

--- src/app/syllabus/syllabus.component.ts
+++ src/app/syllabus/syllabus.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Course } from '../course';
 import { COURSES } from '../staticdata';
 
+
 @Component({
   selector: 'app-syllabus',
   templateUrl: './syllabus.component.html',
